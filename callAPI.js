d002d0542cfef7978ba10e4e73d05790d9d6884e
Tony11306 <tony20020507@gmail.com>
2021-09-18 17:07:22 +0800

    雛型好了 剩下 rowspan 跟 css

---
+++ callAPI.js
@@ -0,0 +1,197 @@
+const CHINESE_WORD_TO_NUMBER = {
+    '一': 1,
+    '二': 2,
+    '三': 3,
+    '四': 4,
+    '五': 5,
+    '六': 6,
+    '日': 7,
+}
+
+const CLASS_MAP = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    'F': 5,
+    '5': 6,
+    '6': 7,
+    '7': 8,
+    '8': 9,
+    '9': 10,
+    'A': 11,
+    'B': 12,
+    'C': 13,
+    'D': 14
+}
+
+async function login() {
+    let data = {
+        account: $("#account").val(),
+        password: $("#password").val()
+    }
+
+    const LOGIN_URL = 'https://chayi-university-system-api.herokuapp.com/login'
+    let webpid1 = ""
+    console.log(data)
+
+    $.ajax({
+        url: LOGIN_URL,
+        type: "POST",
+        dataType: "json",
+        async: false,
+        data: data,
+    }).done(function(response) {
+        webpid1 = response.result.webpid1
+        console.log(`取得 webpid1: ${webpid1}`)
+        $("#status").text("登入成功")
+
+    }).fail(function(data, textStatus, xhr) {
+        if(data.status == 401) {
+            $("#status").text("帳號或密碼錯誤")
+        } else {
+            $("#status").text("無法預期的錯誤")
+        }
+        console.log("error", data.status);
+        console.log("STATUS: "+xhr);
+    })
+
+    return webpid1
+}
+
+async function getCourse(webpid1) {
+    const COURSE_URL = 'https://chayi-university-system-api.herokuapp.com/course/'
+    let allCourse = {}
+    $.ajax({
+        url: COURSE_URL + webpid1,
+        type: "POST",
+        async: false,
+    }).done(function(response) {
+        console.log(response.result)
+        allCourse = response.result
+        $("#status").text("生成成功！")
+
+    }).fail(function(data, textStatus, xhr) {
+        $("#status").text("無法預期的錯誤")
+        console.log("error", data.status);
+        console.log("STATUS: "+xhr);
+    })
+
+    return allCourse
+
+}
+
+function resetTable() {
+    $("#curriculum").html(`
+        <caption>課表</caption>
+        <tbody>
+            <tr>
+                <th>節\日</th>
+                <th>星期一</th>
+                <th>星期二</th>
+                <th>星期三</th>
+                <th>星期四</th>
+                <th>星期五</th>
+            </tr>
+            <tr>
+                <td>第 1 節<br>08:10 ~ 09:00</td>
+            </tr>
+            <tr>
+                <td>第 2 節<br>09:10 ~ 10:00</td>
+            </tr>
+            <tr>
+                <td>第 3 節<br>10:10 ~ 11:00</td>
+            </tr>
+            <tr>
+                <td>第 4 節<br>11:10 ~ 12:00</td>
+            </tr>
+            <tr>
+                <td>第 F 節<br>12:10 ~ 13:00</td>
+            </tr>
+            <tr>
+                <td>第 5 節<br>13:20 ~ 14:10</td>
+            </tr>
+            <tr>
+                <td>第 6 節<br>14:20 ~ 15:10</td>
+            </tr>
+            <tr>
+                <td>第 7 節<br>15:20 ~ 16:10</td>
+            </tr>
+            <tr>
+                <td>第 8 節<br>16:20 ~ 17:10</td>
+            </tr>
+            <tr>
+                <td>第 9 節<br>17:20 ~ 18:10</td>
+            </tr>
+            <tr>
+                <td>第 A 節<br>18:30 ~ 19:15</td>
+            </tr>
+            <tr>
+                <td>第 B 節<br>19:20 ~ 20:05</td>
+            </tr>
+            <tr>
+                <td>第 C 節<br>20:10 ~ 21:55</td>
+            </tr>
+            <tr>
+                <td>第 D 節<br>21:00 ~ 21:45</td>
+            </tr>
+        </tbody>
+    `)
+
+    let rows = $("#curriculum > tbody > tr").get().slice(1)
+
+    rows.forEach(row => {
+        for(let i = 0; i < 5; ++i) {
+            $(row).find("td:last").after("<td></td>")
+        }
+    })
+
+}
+
+async function createCurriculum(courses) {
+    let rows = $("#curriculum > tbody > tr").get()
+
+    courses.forEach(course => {
+        let courseName = course["課程名稱"]
+        let coursesTime = course["上課時間"]
+        coursesTime.forEach(courseTime => {
+
+            let day = CHINESE_WORD_TO_NUMBER[courseTime["星期"]]
+            let startClass
+            let endClass
+
+            if(courseTime["開始節次"] == 'Z') {
+                startClass = 1
+                endClass = 8
+            } else {
+                startClass = CLASS_MAP[courseTime["開始節次"]]
+                endClass = CLASS_MAP[courseTime["結束節次"]]
+            }
+                
+            for(let i = startClass; i <= endClass; ++i) {
+                tds = $(rows[i]).children('td')
+                $(tds[day]).text(courseName)
+            }
+            
+            
+        })
+    })
+
+
+}
+
+async function generate() {
+
+    console.log("now logging in...")
+    let webpid1 = await login()
+
+    if(webpid1.length == 0) {
+        return
+    }
+    let courses = await getCourse(webpid1)
+    resetTable()
+
+    await createCurriculum(courses["所有課程"])
+}
+
+
